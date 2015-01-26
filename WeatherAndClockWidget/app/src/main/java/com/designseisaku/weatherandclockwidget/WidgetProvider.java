02b7b8308fcd4fda949bf08321dfa51e549c2e50
Shohei <shoheiworks@gmail.com>
2015-01-27 01:27:17 +0900

    日付の取得と表示

--- WeatherAndClockWidget/app/src/main/java/com/designseisaku/weatherandclockwidget/WidgetProvider.java
+++ WeatherAndClockWidget/app/src/main/java/com/designseisaku/weatherandclockwidget/WidgetProvider.java
@@ -11,15 +11,29 @@ import java.util.Calendar;
 public class WidgetProvider extends AppWidgetProvider {
     Calendar calendar = Calendar.getInstance();
 
-    String year = Integer.toString(calendar.get(Calendar.YEAR));
+    String[] monthSet ={"JANUARY", "FEBRUARY"};
+    String[] weekSet = {"MonDay","TuesDay","WednesDay","ThursDay","FriDay","SaturDay","SunDay"};
 
-    String date = year+".";
+    String year = Integer.toString(calendar.get(Calendar.YEAR));
+    final int month = calendar.get(Calendar.MONTH);
+    String date = Integer.toString(calendar.get(Calendar.DATE));
+    final int week = calendar.get(Calendar.DAY_OF_WEEK);
+
+    String yearT = year;
+    String dateT = monthSet[month] + "." + date;
+    String weekT = weekSet[week];
+
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widgetlayout);
 
-        views.setTextViewText(R.id.dayText, date);
+        views.setTextViewText(R.id.yearText, yearT);
+        views.setTextViewText(R.id.dateText, dateT);
+        views.setTextViewText(R.id.weekText, weekT);
+
+        appWidgetManager.updateAppWidget(appWidgetIds, views);
+
     }
 }
