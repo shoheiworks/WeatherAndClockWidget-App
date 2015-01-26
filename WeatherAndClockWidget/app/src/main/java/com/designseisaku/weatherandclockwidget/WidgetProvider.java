96931e9e07f1eb65e29d56edebf826b207bdb7e8
Shohei <shoheiworks@gmail.com>
2015-01-27 00:12:41 +0900

    年を表示

--- WeatherAndClockWidget/app/src/main/java/com/designseisaku/weatherandclockwidget/WidgetProvider.java
+++ WeatherAndClockWidget/app/src/main/java/com/designseisaku/weatherandclockwidget/WidgetProvider.java
@@ -1,5 +1,25 @@
 package com.designseisaku.weatherandclockwidget;
 
 
-public class WidgetProvider {
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import java.util.Calendar;
+
+public class WidgetProvider extends AppWidgetProvider {
+    Calendar calendar = Calendar.getInstance();
+
+    String year = Integer.toString(calendar.get(Calendar.YEAR));
+
+    String date = year+".";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widgetlayout);
+
+        views.setTextViewText(R.id.dayText, date);
+    }
 }
