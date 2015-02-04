9c99f26f6f0a3178724ad08d14fc11407bea648a
Shohei <shoheiworks@gmail.com>
2015-02-05 01:27:30 +0900

    とりあえず動く時計が表示できた

--- WeatherAndClockWidget/app/src/main/java/com/designseisaku/weatherandclockwidget/MainActivity.java
+++ WeatherAndClockWidget/app/src/main/java/com/designseisaku/weatherandclockwidget/MainActivity.java
@@ -1,18 +1,103 @@
 package com.designseisaku.weatherandclockwidget;
 
 import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.text.format.Time;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends Activity {
+
+    TextView telopT;
+    private SharedPreferences sharedPre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        telopT = (TextView) findViewById(R.id.telopTV);
+
+        setWeather();
+
+        /*SharedPre*/
+        sharedPre = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPre.edit();
+        editor.putString("temp", telopT.getText().toString());
+        editor.commit();
     }
+
+    public class WeatherService extends Service {
+        @Override
+        public void onCreate(){
+            super.onCreate();
+        }
+
+        @Override
+        public IBinder onBind(Intent intent){
+            return null;
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId){
+            return START_STICKY;
+        }
+
+        @Override
+        public void onDestroy(){
+            super.onDestroy();
+        }
+
+
+    }
+
+
+    private void setWeather() {
+        String urlApi = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400010";
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+
+        mQueue.add(new JsonObjectRequest(Request.Method.GET, urlApi, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String telopJ = response.getString("title");
+                            telopT.setText(telopJ);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // エラー処理 error.networkResponseで確認
+                        // エラー表示など
+                    }
+                }
+        ));
+
+        Intent intent = new Intent(MainActivity.this, WidgetProvider.class);
+        intent.putExtra("temp", telopT.getText().toString());
+    }
+
+
 
 
     @Override
--- WeatherAndClockWidget/app/src/main/java/com/designseisaku/weatherandclockwidget/WidgetProvider.java
+++ WeatherAndClockWidget/app/src/main/java/com/designseisaku/weatherandclockwidget/WidgetProvider.java
@@ -1,45 +1,130 @@
 package com.designseisaku.weatherandclockwidget;
 
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.text.format.Time;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 
+
+import java.net.ContentHandler;
 import java.util.Calendar;
 
+
 public class WidgetProvider extends AppWidgetProvider {
+
     Calendar calendar = Calendar.getInstance();
 
-    String[] monthSet ={"JANUARY", "FEBRUARY"};
-    String[] weekSet = {"","SunDay","MonDay","TuesDay","WednesDay","ThursDay","FriDay","SaturDay"};
+    String[] monthSet = {"JANUARY", "FEBRUARY"};
+    String[] weekSet = {"", "SunDay", "MonDay", "TuesDay", "WednesDay", "ThursDay", "FriDay", "SaturDay"};
 
     String year = Integer.toString(calendar.get(Calendar.YEAR));
     final int month = calendar.get(Calendar.MONTH);
     String date = Integer.toString(calendar.get(Calendar.DATE));
     final int week = calendar.get(Calendar.DAY_OF_WEEK);
-
-    String hour = Integer.toString(calendar.get(Calendar.HOUR_OF_DAY));
-    String mint = Integer.toString(calendar.get(Calendar.MINUTE));
-
-    String yearT = year;
-    String dateT = monthSet[month] + "." + date;
-    String weekT = weekSet[week];
-    String hourT = hour;
-    String mintT = mint;
-    String clock = hourT + ":" + mintT;
+//    String hour = Integer.toString(calendar.get(Calendar.HOUR_OF_DAY));
+//    String mint = Integer.toString(calendar.get(Calendar.MINUTE));
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    public void onUpdate(final Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widgetlayout);
 
-        views.setTextViewText(R.id.yearText, yearT);
-        views.setTextViewText(R.id.dateText, dateT);
-        views.setTextViewText(R.id.weekText, weekT);
-        views.setTextViewText(R.id.timeText, clock);
+        testM(context,appWidgetManager,appWidgetIds);
 
+        views.setTextViewText(R.id.yearText, year);
+        views.setTextViewText(R.id.dateText, monthSet[month] + "." + date);
+        views.setTextViewText(R.id.weekText, weekSet[week]);
+
+//        SharedPreferences tempPre = getSharedPreferences();
+
+//        Intent intent = getIntent();
+//        Intent intent = new Intent(context, MainActivity.class);
+//        String telopInt = intent.getStringExtra("temp");
+
+//        views.setTextViewText(R.id.telop_temperature, aaa);
         appWidgetManager.updateAppWidget(appWidgetIds, views);
 
+    };
+
+
+    private Runnable upDate;
+    private Handler mHandler = new Handler();
+
+    public void testM(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+        upDate = new Runnable() {
+            public void run() {
+                Time time = new Time("Asia/Tokyo");
+                time.setToNow();
+                String timeM = Integer.toString(time.minute);
+                String timeH = Integer.toString(time.hour);
+                String timeT = timeH + "時" + timeM + "分";
+
+                RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.widgetlayout);
+                views.setTextViewText(R.id.timeText,timeT);
+                appWidgetManager.updateAppWidget(appWidgetIds, views);
+                mHandler.removeCallbacks(upDate);
+                mHandler.postDelayed(upDate, 1000);
+            }
+        };
+        mHandler.postDelayed(upDate,1000);
     }
+    class SetTime extends Activity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            
+        }
+    }
+
+
+    /*
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+
+    public static class WidgetService extends Service {
+        @Override
+        public void onStart(Intent intent,int si){
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_TIME_TICK);
+            registerReceiver(timeReceiver, filter);
+
+
+        }
+        @Override
+        public IBinder onBind(Intent intent){
+            return null;
+        }
+    }
+*/
+    public static BroadcastReceiver timeReceiver = new BroadcastReceiver() {
+        int minits = 110;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String ac = intent.getAction();
+            if (ac.equals(Intent.ACTION_TIME_TICK)){
+                minits = intent.getIntExtra("minits",0);
+            }
+
+        }
+    };
+
 }
